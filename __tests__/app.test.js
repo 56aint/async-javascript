47749023247b104fa355e69a108749d656832976
aar.56aint <aaron.hays_7@yahoo.com>
2020-05-03 02:13:36 +0100

    first commit, personalised jokes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,46 +1,115 @@
+/** * @jest-environment node */
 const request = require('supertest');
-const app = require('../src/app');
 const nock = require('nock');
+const app = require('../src/app');
 
-describe('My jokes API', () => {
-  describe('API GET /', () => {
-    it('should respond with a welcome message', done => {
-      request(app)
-        .get('/')
-        .then(res => {
-          expect(res.statusCode).toEqual(200);
-          expect(res.body.message).toEqual('Welcome to my jokes API!');
-          done();
-        });
-    });
+describe('MY JOKES API, GET /', () => {
+  it('should respond with a welcome message', done => {
+    request(app)
+      .get('/')
+      .then(res => {
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.message).toEqual('Welcome to my jokes API!');
+        done();
+      });
   });
+});
+
+describe('GET /jokes', () => {
   it('that this is the jokes endpoint', done => {
+    const mockResponse = {
+      type: 'success',
+      value: [
+        {
+          id: 1,
+          joke: 'i am a joke',
+          categories: [],
+        },
+        {
+          id: 2,
+          joke: 'i am another joke',
+          categories: [],
+        },
+      ],
+    };
+
+    nock('https://api.icndb.com')
+      .get('/jokes')
+      .reply(200, mockResponse);
+
     request(app)
       .get('/jokes')
       .then(res => {
         expect(res.statusCode).toEqual(200);
-        expect(res.body.jokes).toEqual(
-          'You have reached the jokes endpoint, please laugh with care',
-        );
+        expect(res.body.jokes).toEqual([
+          {
+            id: 1,
+            joke: 'i am a joke',
+            categories: [],
+          },
+          {
+            id: 2,
+            joke: 'i am another joke',
+            categories: [],
+          },
+        ]);
         done();
       });
   });
+});
+
+describe('GET, /jokes/random', () => {
   it('should respond with a random joke message', done => {
+    const mockResponse = {
+      type: 'success',
+      value: {
+        id: 115,
+        joke: 'i am a random joke',
+        categories: [],
+      },
+    };
+
+    nock('https://api.icndb.com')
+      .get('/jokes/random')
+      .query({ exclude: '[explicit]' })
+      .reply(200, mockResponse);
+
     request(app)
       .get('/jokes/random')
       .then(res => {
         expect(res.statusCode).toEqual(200);
-        expect(res.body.message).toEqual('No jokes here yet, try again!');
+        expect(res.body.randomJoke).toEqual({
+          categories: [],
+          id: 115,
+          joke: 'i am a random joke',
+        });
+        console.log(res.body.randomJoke);
         done();
       });
   });
-  it('test to get jokes with personalised names ', done => {
+});
+
+describe('GET /jokes/random/personal', () => {
+  it('test to get jokes with personalised names ', async () => {
+    const mockResponse = {
+      type: 'success',
+      value: {
+        id: 141,
+        joke: 'random joke about manchester codes',
+        categories: [],
+      },
+    };
+
+    nock('https://api.icndb.com')
+      .get('/jokes/random')
+      .query({ exclude: '[explicit]', firstName: 'manchester', lastName: 'codes' })
+      .reply(200, mockResponse);
+
     request(app)
-      .get('/jokes/random/personal/Saint/Last')
+      .get('/jokes/random/personal/manchester/codes')
       .then(res => {
         expect(res.statusCode).toEqual(200);
-        expect(res.body.message).toEqual('Hi, i can personalise your jokes!');
-        done();
+        expect(res.body.personalJoke).toEqual(mockResponse.value);
       });
   });
 });
--- src/controllers.js
+++ src/controllers.js
@@ -1,5 +1,6 @@
 // import { re } from 'prettier';
 const request = require('request');
+const axios = require('axios');
 
 const mainController = (req, res) =>
   res.send({
@@ -8,25 +9,50 @@ const mainController = (req, res) =>
 
 const jokesController = (req, res) =>
   request('https://api.icndb.com/jokes', (error, jokesApiResponse) => {
+    // making request to jokesApi
     if (error) {
       console.log(error);
     }
-    const parsedResponse = JSON.parse(jokesApiResponse.body);
-
+    const parsedResponse = JSON.parse(jokesApiResponse.body); // response from jokesApi is JSON.stringified so i JSON.parse it
+    // console.log(parsedResponse);
     res.send({ jokes: parsedResponse.value });
   });
 /* res.send({
     message: 'This is the jokes endpoint',
   }); */
 
-const randomJokesController = (req, res) =>
-  res.send({
-    message: 'No jokes here yet, try again!',
-  });
-const personalJokeController = (req, res) =>
+const randomJokesController = (req, res) => {
+  axios
+    .get('https://api.icndb.com/jokes/random?exclude=[explicit]')
+    .then(response => {
+      res.send({ randomJoke: response.data.value });
+    })
+    .catch(error => {
+      // eslint-disable-next-line
+          console.log(error);
+    });
+};
+/* res.send({
+  randomJoke: 'No jokes here yet, try again!',
+}); */
+
+const personalJokeController = async (req, res) => {
+  const { first, last } = req.params;
+
+  try {
+    const response = await axios.get(
+      `https://api.icndb.com/jokes/random?firstName=${first}&lastName=${last}&exclude=[explicit]`,
+    );
+
+    return res.send({ personalJoke: response.data.value });
+  } catch (error) {
+    console.log(error);
+  }
+};
+/* const personalJokeController = (req, res) =>
   res.json({
     message: 'Hi, i can personalise your jokes!',
-  });
+  }); */
 
 module.exports = {
   mainController,
