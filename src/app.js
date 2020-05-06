dc716540a898f653bdf4b00f78146e2b004582cb
aar.56aint <aaron.hays_7@yahoo.com>
2020-05-06 17:12:47 +0100

    removed remaining useless libraries

--- src/app.js
+++ src/app.js
@@ -1,18 +1,10 @@
-// import { mainController } from './controllers';
 const express = require('express');
 
-const {
-  // mainController,  // replaced with homepage @ public folder
-  jokesController,
-  randomJokesController,
-  personalJokeController,
-} = require('./controllers');
+const { jokesController, randomJokesController, personalJokeController } = require('./controllers');
 
 const app = express();
 
 app.use(express.static('public'));
-
-// app.get('/', mainController); // now using express.static to get (/) 'the homepage'
 
 app.get('/jokes', jokesController);
 
